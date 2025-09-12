ccaa9fea2a7132431d9aaeb44390be01fb081a2f
Kean Paderes <kean.paderes@gmail.com>
2025-09-12 16:49:18 +0800

    feat(terraform): Comment out AKS resources

--- terraform/default/main.tf
+++ terraform/default/main.tf
@@ -5,20 +5,20 @@ module "az_rg" {
   project_location = var.project_location
 }
 
-// SSH Key for AKS
-module "az_k8s_ssh" {
-  source         = "../modules/common/az_k8s_ssh"
-  project_prefix = var.project_prefix
-  rg_id          = module.az_rg.rg_id
-  rg_location    = module.az_rg.rg_location
-}
+# // SSH Key for AKS
+# module "az_k8s_ssh" {
+#   source         = "../modules/common/az_k8s_ssh"
+#   project_prefix = var.project_prefix
+#   rg_id          = module.az_rg.rg_id
+#   rg_location    = module.az_rg.rg_location
+# }
 
-// AKS Cluster
-module "az_k8s" {
-  source              = "../modules/common/az_k8s"
-  rg_name             = module.az_rg.rg_name
-  rg_location         = module.az_rg.rg_location
-  k8s_node_count      = 2
-  k8s_username        = "azureadmin"
-  ssh_public_key_data = module.az_k8s_ssh.key_data
-}
+# // AKS Cluster
+# module "az_k8s" {
+#   source              = "../modules/common/az_k8s"
+#   rg_name             = module.az_rg.rg_name
+#   rg_location         = module.az_rg.rg_location
+#   k8s_node_count      = 2
+#   k8s_username        = "azureadmin"
+#   ssh_public_key_data = module.az_k8s_ssh.key_data
+# }
