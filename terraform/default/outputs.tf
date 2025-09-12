ce5afe2ffd438551834b3d92b4df8f4ec197fe30
Kean Paderes <kean.paderes@gmail.com>
2025-09-12 16:51:48 +0800

    chore(terraform): Comment out AKS resources

--- terraform/default/outputs.tf
+++ terraform/default/outputs.tf
@@ -1,38 +1,38 @@
-output "kubernetes_cluster_name" {
-  value = module.az_k8s.kubernetes_cluster_name
-}
+# output "kubernetes_cluster_name" {
+#   value = module.az_k8s.kubernetes_cluster_name
+# }
 
-output "client_certificate" {
-  value     = module.az_k8s.client_certificate
-  sensitive = true
-}
+# output "client_certificate" {
+#   value     = module.az_k8s.client_certificate
+#   sensitive = true
+# }
 
-output "client_key" {
-  value     = module.az_k8s.client_key
-  sensitive = true
-}
+# output "client_key" {
+#   value     = module.az_k8s.client_key
+#   sensitive = true
+# }
 
-output "cluster_ca_certificate" {
-  value     = module.az_k8s.cluster_ca_certificate
-  sensitive = true
-}
+# output "cluster_ca_certificate" {
+#   value     = module.az_k8s.cluster_ca_certificate
+#   sensitive = true
+# }
 
-output "cluster_password" {
-  value     = module.az_k8s.cluster_password
-  sensitive = true
-}
+# output "cluster_password" {
+#   value     = module.az_k8s.cluster_password
+#   sensitive = true
+# }
 
-output "cluster_username" {
-  value     = module.az_k8s.cluster_username
-  sensitive = true
-}
+# output "cluster_username" {
+#   value     = module.az_k8s.cluster_username
+#   sensitive = true
+# }
 
-output "host" {
-  value     = module.az_k8s.host
-  sensitive = true
-}
+# output "host" {
+#   value     = module.az_k8s.host
+#   sensitive = true
+# }
 
-output "kube_config" {
-  value     = module.az_k8s.kube_config
-  sensitive = true
-}
+# output "kube_config" {
+#   value     = module.az_k8s.kube_config
+#   sensitive = true
+# }
